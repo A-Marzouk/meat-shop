578dafa37ac9481f7e5d90423bcc450a47718aca
Ahmed-pc <perfect_ahmed@yahoo.com>
2020-03-27 01:27:42 +0200

    Add service worker

--- public/js/app.js
+++ public/js/app.js
@@ -33323,8 +33323,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\OSPanel\domains\meat-shop\halalmeatinukraine.com\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\OSPanel\domains\meat-shop\halalmeatinukraine.com\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\OSPanel\domains\meat-shop\local.meat-shop.me\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\OSPanel\domains\meat-shop\local.meat-shop.me\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
